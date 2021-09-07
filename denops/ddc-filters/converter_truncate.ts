f8c9f51b807b9624955bf6d50420bb3ef8c849c6
matsui54 <haru.matu9168@gmail.com>
2021-09-07 10:06:19 +0900

    First code

---
+++ denops/ddc-filters/converter_truncate.ts
@@ -0,0 +1,115 @@
+import {
+  BaseFilter,
+  Candidate,
+} from "https://deno.land/x/ddc_vim@v0.5.0/types.ts#^";
+
+import { eastAsianWidth } from "https://esm.sh/eastasianwidth";
+
+export function charwidth(c: string): number {
+  const wc = eastAsianWidth(c) as string;
+  return wc.match(/F|W/) ? 2 : 1;
+}
+
+export function strwidth(str: string): number {
+  let width = 0;
+  for (const c of str) {
+    width += charwidth(c);
+  }
+  return width;
+}
+
+export function truncate(
+  str: string,
+  maxWidth: number,
+  reverse = false,
+): string {
+  if (str.length < maxWidth / 2) return str;
+  if (strwidth(str) <= maxWidth) return str;
+
+  let width = 0;
+  let ret = "";
+  for (let i = 0; i < str.length; i++) {
+    const c = reverse ? str[str.length - i - 1] : str[i];
+    const wc = charwidth(c);
+    if (width + wc > maxWidth) break;
+    ret += c;
+    width += wc;
+  }
+  return ret;
+}
+
+export function truncateSkipping(
+  str: string | undefined,
+  maxWidth: number,
+  footer: string,
+  footerLen: number,
+): string {
+  if (!str) return "";
+  if (str.length < maxWidth / 2) return str;
+  if (strwidth(str) <= maxWidth) return str;
+
+  footer += str.slice(-truncate(str, footerLen, true).length);
+  return truncate(str, maxWidth - strwidth(footer)) + footer;
+}
+
+type Params = {
+  maxAbbrWidth: number;
+  maxInfoWidth: number;
+  maxKindWidth: number;
+  maxMenuWidth: number;
+};
+
+export class Filter extends BaseFilter {
+  filter(args: {
+    filterParams: Record<string, unknown>;
+    completeStr: string;
+    candidates: Candidate[];
+  }): Promise<Candidate[]> {
+    const param = args.filterParams as Params;
+    for (const candidate of args.candidates) {
+      if (param.maxAbbrWidth > 0) {
+        candidate.abbr = truncateSkipping(
+          candidate.abbr ? candidate.abbr : candidate.word,
+          param.maxAbbrWidth,
+          "..",
+          param.maxAbbrWidth / 3,
+        );
+      }
+      if (param.maxInfoWidth > 0) {
+        candidate.info = truncateSkipping(
+          candidate.info,
+          param.maxInfoWidth,
+          "..",
+          param.maxInfoWidth / 3,
+        );
+      }
+      if (param.maxKindWidth > 0) {
+        candidate.kind = truncateSkipping(
+          candidate.kind,
+          param.maxKindWidth,
+          "..",
+          param.maxKindWidth / 3,
+        );
+      }
+      if (param.maxMenuWidth > 0) {
+        candidate.menu = truncateSkipping(
+          candidate.menu,
+          param.maxMenuWidth,
+          "..",
+          param.maxMenuWidth / 3,
+        );
+      }
+    }
+    return Promise.resolve(args.candidates);
+  }
+
+  params(): Record<string, unknown> {
+    const params: Params = {
+      maxAbbrWidth: 80,
+      maxInfoWidth: 200,
+      maxKindWidth: 40,
+      maxMenuWidth: 40,
+    };
+    return params as unknown as Record<string, unknown>;
+  }
+}
---
+++ denops/ddc-filters/tests/truncate_test.ts
@@ -0,0 +1,11 @@
+import {
+  assertEquals,
+} from "https://deno.land/std@0.106.0/testing/asserts.ts#^";
+import { truncateSkipping } from "../converter_truncate.ts";
+
+Deno.test("truncateSkipping", () => {
+  assertEquals(truncateSkipping("foo bar", 3, "..", 3), "..bar");
+  assertEquals(truncateSkipping("foo bar", 6, "..", 3), "f..bar");
+  assertEquals(truncateSkipping("fooあい", 5, "..", 3), "f..い");
+  assertEquals(truncateSkipping("あいうえ", 6, "..", 2), "あ..え");
+});
