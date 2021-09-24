40d109b21504b972e735efa79649984d33aad57a
matsui54 <haru.matu9168@gmail.com>
2021-09-24 23:28:42 +0900

    Update deps

--- denops/@ddc-filters/tests/truncate_test.ts
+++ denops/@ddc-filters/tests/truncate_test.ts
@@ -1,6 +1,6 @@
 import {
   assertEquals,
-} from "https://deno.land/std@0.107.0/testing/asserts.ts#^";
+} from "https://deno.land/std@0.108.0/testing/asserts.ts#^";
 import { truncateSkipping } from "../converter_truncate.ts";
 
 Deno.test("truncateSkipping", () => {
