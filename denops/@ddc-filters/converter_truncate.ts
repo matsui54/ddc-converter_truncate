93ad514806ee8b00df2fce025258f3356bdf58c7
matsui54 <haru.matu9168@gmail.com>
2023-01-01 14:56:45 +0900

    📦 Update Deno dependencies

--- denops/@ddc-filters/converter_truncate.ts
+++ denops/@ddc-filters/converter_truncate.ts
@@ -1,4 +1,4 @@
-import { BaseFilter, Item } from "https://deno.land/x/ddc_vim@v3.1.0/types.ts";
+import { BaseFilter, Item } from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
 
 import { eastAsianWidth } from "https://esm.sh/eastasianwidth";
 
--- denops/@ddc-filters/tests/truncate_test.ts
+++ denops/@ddc-filters/tests/truncate_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.161.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
 import { truncateSkipping } from "../converter_truncate.ts";
 
 Deno.test("truncateSkipping", () => {
