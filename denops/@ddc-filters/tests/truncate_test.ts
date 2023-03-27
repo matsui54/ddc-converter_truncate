0b0cce6668cbea3c21ca733f99a68799883fe357
matsui54 <haru.matu9168@gmail.com>
2023-03-27 17:28:49 +0900

    📦 Update Deno dependencies

--- denops/@ddc-filters/tests/truncate_test.ts
+++ denops/@ddc-filters/tests/truncate_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.171.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.181.0/testing/asserts.ts";
 import { truncateSkipping } from "../converter_truncate.ts";
 
 Deno.test("truncateSkipping", () => {
